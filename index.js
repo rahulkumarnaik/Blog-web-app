f8921a424d272fa75f1e3dbd2ebf7726af0c8e94
rahulkumarnaik <22052840@kiit.ac.in>
2025-04-30 21:46:07 +0530

    Remove node_modules from version control

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import methodOverride from "method-override";
-import postRouter from "./routes/post.js";
+import postRouter from "./routes/post.js"; 
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.set("views", "./view");
+app.set("views", "./views"); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("Public"));
+
+app.use(express.static("public"));
 app.use(methodOverride("_method"));
 
 // Use routes
